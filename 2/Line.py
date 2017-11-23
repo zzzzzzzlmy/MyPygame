0ee69222a934444c613e50c4ae71547c673c6452
Mymlw Lau-zzz <zzzzzzzlmy@users.noreply.github.com>
2017-11-23 21:00:16 +0800

    Create Line.py

---
+++ 2/Line.py
@@ -0,0 +1,23 @@
+import pygame, sys
+from pygame.locals import *
+
+pygame.init()
+
+screen = pygame.display.set_mode((600, 500))
+pygame.display.set_caption("Drawing Lines")
+
+while True:
+    for event in pygame.event.get():
+        if event.type in (QUIT, KEYDOWN):
+            sys.exit()
+
+    screen.fill((0, 80, 0))
+
+    #draw a line
+    color = 100, 255, 200
+    width = 8
+    pygame.draw.line(screen, color, (100, 100), (500, 400), width)
+
+    pygame.display.update()
+
+
